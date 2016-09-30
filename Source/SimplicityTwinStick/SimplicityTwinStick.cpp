a68cec3266ade55685dfadc2fdbf1a9143403bda
Josh Martin <jim3559@rit.edu>
2016-09-30 18:26:25 -0400

    Switching project to blueprints
    Should set everything up properly. Results may vary

--- Source/SimplicityTwinStick/SimplicityTwinStick.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#include "SimplicityTwinStick.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimplicityTwinStick, "SimplicityTwinStick" );
-
-DEFINE_LOG_CATEGORY(LogSimplicityTwinStick)
- 
